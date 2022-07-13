347f88eeb0a102308d7615181ecf1606ea6455d1
Anne Caroline <carolaragao94@gmail.com>
2022-07-13 16:57:11 -0300

    Método para salvar o usuário no banco

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
@@ -8,10 +8,22 @@ import { User } from '../models/user';
 })
 export class UserService {
   apiUrl = 'https://sheet.best/api/sheets/a5719fee-e0a0-42a8-bce3-c9b66a0166b2';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+
   constructor(private httpClient: HttpClient) { }
 
   //Retorna a lista de usuarios READ
   getUsers():Observable<User[]> {
     return this.httpClient.get<User[]>(this.apiUrl)
+  }
+
+  //Salva usuario no banco CREATE
+  postUser(user: User):Observable<User> {
+    return this.httpClient.post<User>(this.apiUrl, user, this.httpOptions);
   }
 }
