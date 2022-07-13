1a12c3933a94010bd63bdfcea49fbedeb86c03b3
Anne Caroline <carolaragao94@gmail.com>
2022-07-13 15:04:36 -0300

    Componente lista de usuários

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
 
 const routes: Routes = [
-  
+  {path: '', component: UsersListComponent}
 ];
 
 @NgModule({
